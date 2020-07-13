1db411d963ee64cee832648296f776692b68a3aa
yoori choi <vndn6832@daum.net>
2020-07-13 20:05:30 +0900

    2252번 줄 세우기 > 위상정렬로 해결

--- Algorithm/Algorithm/2252번_줄 세우기.cpp
+++ Algorithm/Algorithm/2252번_줄 세우기.cpp
@@ -2,31 +2,54 @@
 #include<queue>
 #include<vector>
 using namespace std;
+
+vector<int> node[32001];
+int inDegree[32001];
+int result[32001];
+int n;
+int m;
+void sort() {
+	queue<int> q;
+	//진입차수가 0인거 큐에 삽입
+	for (int i = 1; i <= n; i++) {
+		if (inDegree[i] == 0) q.push(i);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		int x = q.front();
+		q.pop();
+		result[i]=x;
+		for (int i = 0; i < node[x].size(); i++) {
+			int val = node[x][i];
+			if (--inDegree[val] == 0) {
+				q.push(val);
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		cout << result[i] << " ";
+	}
+
+
+}
 int main() {
-	int n;
-	int m;
+	
 	cin >> n >> m;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
-	int val[32001] = {0,};
+	
 
 
 	for (int i = 0; i < m; i++) {
 		int a, b;
 		cin >> a >> b;
+	    
+		node[a].push_back(b);
+		inDegree[b]++;
+		
+
+	}
 	
-		pq.push({ val[a],a });
-		pq.push({val[a]+1,b });
-
-	}
-	int preVal = 0;
-	while (!pq.empty()) {
-		int nowVal = pq.top().second;
-		if(nowVal != preVal)
-			cout << nowVal << " ";
-		pq.pop();
-		preVal = nowVal;
-
-	}
+	sort();
 
 
 }
