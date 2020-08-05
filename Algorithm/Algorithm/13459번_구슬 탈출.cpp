6afa52eedfa18c90f82c6e62cc604deab76e2f68
yoori choi <vndn6832@daum.net>
2020-08-05 19:42:45 +0900

    13459번 구슬 탈출 > 구조체 : 이동하는게 2개이니깐 > while(qsize--) : 이동 횟수 카운트해야 하니깐 > 4방향 이동 - *이동 한칸씩 X -> 굴러감(멈출때까지) > 파란색 구슬 빠지면 continue > 두 구슬이 겹칠 경우 고려하기* > cnt++ (10번 이하)

--- Algorithm/Algorithm/13459번_구슬 탈출.cpp
+++ Algorithm/Algorithm/13459번_구슬 탈출.cpp
@@ -7,9 +7,10 @@ char map2[10][10];
 int redX, redY;
 int blueX, blueY;
 int zeroX, zeroY;
-int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,1,0,-1 };
-struct ball{
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+bool visited[10][10][10][10];//방문
+struct ball {
 	int rx;
 	int ry;
 	int bx;
@@ -25,55 +26,104 @@ struct ball{
 bool success = false;
 void bfs() {
 	queue<ball> pos;
-	pos.push(ball(redX,redY,blueX,blueY));
-
+	pos.push(ball(redX, redY, blueX, blueY));
+	int cnt = 0;
+	//cout << "zero " << zeroX << "," << zeroY << endl;
 	while (!pos.empty()) {
-		int rx = pos.front().rx;
-		int ry = pos.front().ry;
-		int bx = pos.front().bx;
-		int by = pos.front().by;
-
-		pos.pop();
+		int size = pos.size();
 		
-		if (rx == zeroX && ry == zeroY && bx != zeroX && by != zeroY) {
-			success = true;
-			return;
-		}
-		else if (bx == zeroX && by == zeroY) {
-			success = false;
-		}
+		if (cnt > 10) return; //10번 이하로 이동
 
-		for (int i = 0; i < 4; i++) {
+		while (size--) { //*이동횟수 구해야 하니깐
+			int rx = pos.front().rx;
+			int ry = pos.front().ry;
+			int bx = pos.front().bx;
+			int by = pos.front().by;
 
-			int Nrx = rx + dx[i];
-			int Nry = ry + dy[i];
-			int Nbx = bx + dx[i];
-			int Nby = by + dy[i];
+			pos.pop();
 
-			if (Nrx <= 0 || Nrx >= n || Nbx <= 0 || Nby >= m) continue;
-			if (map[Nrx][Nry] == '#') {
-				Nrx = rx;
-				Nry = ry;
-			}
-
-			if (map[Nbx][Nby] == '#') {
-				Nbx = bx;
-				Nby = by;
-			}
-
-
-			if (Nrx == zeroX && Nry == zeroY && Nbx != zeroX && Nby != zeroY) {
+			/*
+			cout << rx << "," << ry << ": " << success << endl;
+			cout << bx << "," << by << ": " << success << endl;
+			cout << endl;
+			*/
+			if (rx == zeroX && ry == zeroY) {
 				success = true;
 				return;
 			}
-			else if (Nbx == zeroX && Nby == zeroY) {
-				success = false;
+
+			for (int i = 0; i < 4; i++) {
+
+				int Nrx = rx + dx[i];
+				int Nry = ry + dy[i];
+				int Nbx = bx + dx[i];
+				int Nby = by + dy[i];
+				while (1) {
+
+					if (map[Nrx][Nry] == '#') {
+						Nrx -= dx[i];
+						Nry -= dy[i];
+						break;
+					}
+					if (map[Nrx][Nry] == 'O') {
+						break;
+					}
+					Nrx += dx[i];
+					Nry += dy[i];
+				}
+
+				while (1) {
+					if (map[Nbx][Nby] == '#') {
+						Nbx -= dx[i];
+						Nby -= dy[i];
+						break;
+					}
+					if (map[Nbx][Nby] == 'O') {
+						break;
+					}
+					Nbx += dx[i];
+					Nby += dy[i];
+				}
+				if (Nbx == zeroX && Nby == zeroY) {
+					success = false;
+					continue; //*추가
+				}
+				if (Nrx == Nbx && Nry == Nby) {
+					if (i == 0) {//왼
+						if (rx > bx) Nrx++;
+						else Nbx++;
+					}
+					else if (i == 1) {//오
+						if (rx > bx) Nbx--;
+						else Nrx--;
+					}
+					else if (i == 2) {//아래
+						if (ry > by) Nry++;
+						else Nby++;
+					}
+					else {//위
+						if (ry > by) Nby--;
+						else Nry--;
+					}
+				}
+
+
+
+
+				if (visited[Nrx][Nry][Nbx][Nby] != true) {
+					visited[Nrx][Nry][Nbx][Nby] = true;
+					/*
+					cout << Nrx << "," << Nry << ": " << success<< endl;
+					cout << Nbx << "," << Nby << ": " << success << endl;
+					cout << endl;
+						*/
+					pos.push(ball(Nrx, Nry, Nbx, Nby));
+				}
+
 			}
 
-			pos.push(ball(Nrx, Nry, Nbx, Nby));
-
-
 		}
+		cnt++;//** cnt 증가시키는 위치 매우중요
 	}
 
 }
@@ -97,16 +147,9 @@ int main() {
 		}
 	}
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cout << map[i][j];
-		}
-		cout << endl;
-	}
-	
 	bfs();
 
-	if (success == false) {
+	if (success != true) { //**
 		cout << 0 << endl;
 	}
 	else {
