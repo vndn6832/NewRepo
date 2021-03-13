cdb5f6c81c9cd3b41c7a2c6bf2d1ed1843e11771
yoori choi <vndn6832@likelion.org>
2021-03-13 21:34:35 +0900

    인프런 77번 친구인가? > Union&Find > 9 7 1 2 2 3 3 4 4 5 6 7 7 8 8 9 3 8 NO

---
+++ Algorithm/Algorithm/INF_77번_친구인가(Union Find 자료구조).cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int unf[1001];
+int Find(int v) { //v의 집합번호 return
+	if (v == unf[v]) return v;
+	else return Find(unf[v]);
+}
+void Union(int a, int b) {
+	a = Find(a);
+	b = Find(b);
+	if (a != b) unf[a] = b;
+}
+int main() {
+	int n, m, a, b, fa, fb;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		unf[i] = i; //초기화
+	}
+
+	for (int i = 1; i <= m; i++) {
+		cin >> a >> b;
+		Union(a, b);
+	}
+	cin >> a >> b;
+	fa = Find(a);
+	fb = Find(b);
+	if (fa == fb) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+}
