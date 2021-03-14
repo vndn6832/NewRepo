c5b86c7783de40ee9771f2a05450dec7c3705d97
yoori choi <vndn6832@likelion.org>
2021-03-14 15:20:44 +0900

    인프런 79번 원더랜드(Prime MST 알고리즘: priority-queue 활용) > Prime 알고리즘, 우선순위큐 사용 > 우선순위 큐 선언 priority_queue<info,vector<info>,cmp> pq; > 우선순위 큐 정렬 기준 - 최소 힙 struct cmp { //우선순위 큐 정렬 기준 설정  bool operator()(info i1, info i2) {   return i1.val > i2.val;  } }; > 1노드 부터 우선순위 큐에 넣고 > 큐가 빌때까지 반복    > 가중치가 최소인 걸 선택    > 그 노드에 연결된 노드들 우선순위 큐에 넣기       *방문 안 한 노드만*

---
+++ Algorithm/Algorithm/INF_79번_원더랜드(Prime MST 알고리즘 priority_queue 활용).cpp
@@ -0,0 +1,51 @@
+﻿#include<stdio.h>
+#include<queue>
+using namespace std;
+int V, E;
+struct info{
+	int v;
+	int val;
+	info(int a, int b) {
+		v = a;
+		val = b;
+	}
+};
+struct cmp { //우선순위 큐 정렬 기준 설정
+	bool operator()(info i1, info i2) {
+		return i1.val > i2.val;
+	}
+};
+bool check[101];
+vector<pair<int,int>> map[101];
+priority_queue<info,vector<info>,cmp> pq; //우선순위 큐 선언 방법
+int ans = 0;
+int main() {
+	scanf_s("%d %d", &V, &E);
+
+	for (int i = 0; i < E; i++) {
+		int a, b, c;
+		scanf_s("%d %d %d", &a, &b, &c);
+		map[a].push_back(make_pair(b, c));
+		map[b].push_back(make_pair(a, c));
+
+	}
+
+	pq.push(info(1, 0));
+	while (!pq.empty()) {
+		int v = pq.top().v;
+		int val = pq.top().val;
+		pq.pop();
+		if (check[v] != true) { //*** 트리의 원소가 아니면 - 사이클 생기면 안됨
+			check[v] = true;
+			ans += val;
+			for (int i = 0; i < map[v].size(); i++) {
+				if (check[map[v][i].first] != true) {
+					pq.push(info(map[v][i].first, map[v][i].second));
+				}
+			}
+		}
+	}
+
+	printf("%d\n", ans);
+	return 0;
+}
