eb52e5889b1866e07b562354cf7327c7548d782c
yoori choi <vndn6832@likelion.org>
2021-03-18 23:33:48 +0900

    프로그래머스 실패율 > double rate = (double)cnt[i] / (double)(stages.size() - tmp);//***나누기 > 테스트 케이스 6,7,9, 13, 24 실패!!!

---
+++ Algorithm/Algorithm/프로그래머스_실패율.cpp
@@ -0,0 +1,46 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int cnt[502];
+struct info {
+	int stage;
+	double rate;
+	info(int a, double b) {
+		stage = a;
+		rate = b;
+	}
+};
+bool compare(info i1, info i2) {
+	if (i1.rate == i2.rate)
+		return i1.stage < i2.stage;
+	else
+		return i1.rate > i2.rate;
+}
+vector<info> v;
+vector<int> solution(int N, vector<int> stages) {
+	vector<int> answer;
+
+	for (int i = 0; i < stages.size(); i++) {
+		cnt[stages[i]]++;
+	}
+
+	int tmp = 0;
+	for (int i = 1; i <= N; i++) {
+		//cout << "* "<<cnt[i] <<", "<<stages.size()- tmp << endl;
+		double rate = (double)cnt[i] / (double)(stages.size() - tmp);//***나누기
+		//cout << "=> " <<rate << endl;
+		tmp += cnt[i];
+		v.push_back(info(i, rate));
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < v.size(); i++) {
+		//cout << v[i].stage << ", "<<v[i].rate << endl;
+		answer.push_back(v[i].stage);
+	}
+
+	return answer;
+}
