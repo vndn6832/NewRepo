bf0b696d6c2d32fd5046934eca56986d9adeaa33
yoori choi <vndn6832@likelion.org>
2021-03-28 19:15:34 +0900

    .

--- Algorithm/Algorithm/20056번_마법사 상어와 파이어볼.cpp
+++ Algorithm/Algorithm/20056번_마법사 상어와 파이어볼.cpp
@@ -22,7 +22,7 @@ int dy[8] = { 0,1,1,1,0,-1,-1,-1 };
 void copyMap(vector<fire> a[50][50], vector<fire> b[50][50]) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			map[i][j].clear(); //***Áö¿öÁà¾ß ÇÔ
+			a[i][j].clear(); //***Áö¿öÁà¾ß ÇÔ
 			for (int k = 0; k < b[i][j].size(); k++) {
 				a[i][j].push_back(b[i][j][k]);
 			}
@@ -46,15 +46,15 @@ int main() {
 	for (int i = 1; i <= M; i++) {
 		int a, b, c, d, e;
 		cin >> a >> b >> c >> d >> e;
-		map[a-1][b-1].push_back(fire(a-1,b-1,c,d,e));
+		map[a - 1][b - 1].push_back(fire(a - 1, b - 1, c, d, e));
 	}
 
-	
+
 	while (K != 0) {
 		vector<fire> map2[50][50];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (map[i][j].size() ==0) continue; //¼Ò¸ê µÆÀ¸¸é
+				if (map[i][j].size() == 0) continue; //¼Ò¸ê µÆÀ¸¸é
 				for (int k = 0; k < map[i][j].size(); k++) {
 					int x = map[i][j][k].x;
 					int y = map[i][j][k].y;
@@ -62,14 +62,18 @@ int main() {
 					int speed = map[i][j][k].speed;
 					int dir = map[i][j][k].dir;
 
-					//ÁÂÇ¥ °ªÀÌ À½¼ö ÀÏ¶§
 
 					speed = speed % N;
-					int nx = (x + (dx[dir] * speed)+N)%N;
-					int ny = (y + (dy[dir] * speed)+N)%N;
+
+
+					int nx = (x + (dx[dir] * speed) + N) % N;
+					int ny = (y + (dy[dir] * speed) + N) % N;
+
+
 					//cout << nx << ", " << ny << endl;
-					
-					map2[nx][ny].push_back(fire(nx,ny,m,speed,dir));
+
+
+					map2[nx][ny].push_back(fire(nx, ny, m, speed, dir));
 				}
 			}
 
@@ -82,7 +86,7 @@ int main() {
 					bool odd = false; //È¦¼ö
 					bool even = false; //Â¦¼ö
 					for (int k = 0; k < map2[i][j].size(); k++) {
-					
+
 						sumM += map2[i][j][k].m;
 						sumS += map2[i][j][k].speed;
 						if (map2[i][j][k].dir % 2 == 0)
@@ -90,10 +94,11 @@ int main() {
 						else
 							odd = true;
 					}
-					
+
 
 					sumM = sumM / 5;
 					sumS = sumS / map2[i][j].size();
+
 
 					map2[i][j].clear();
 					if (sumM == 0)continue;
@@ -110,33 +115,34 @@ int main() {
 						map2[i][j].push_back(fire(i, j, sumM, sumS, 6));
 					}
 
-					
+
 
 				}
-				
-			
+
+
 			}
 		}
 
 		copyMap(map, map2);
 
 		//print();
-		
+
 		K--;
 
 		//Áú·® °è»ê
-		if (K != 0) continue;
-
+		//if (K != 0) continue;
+		ans = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				for (int k = 0; k < map[i][j].size(); k++) {
-					ans += map[i][j][k].m;
+					if (map[i][j][k].m > 0)
+						ans += map[i][j][k].m;
 				}
 			}
 		}
 
-		
-		
+		if (ans == 0) break;
+
 	}
 
 	cout << ans << endl;
