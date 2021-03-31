b769f0a99ffaa0cb6d60667086fb4bb56874ecc2
yoori choi <vndn6832@likelion.org>
2021-03-31 22:30:33 +0900

    13458번 시험 감독 > 답이 int 범위 벗어날 수 있으므로 long long > continue 문 보다 if 문

---
+++ Algorithm/Algorithm/13458번_시험 감독.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int N;
+int v[1000001]; //벡터 대신 배열
+int a, b;
+long long ans; //int 범위 벗어날 수도 있으므로 long long
+int main() {
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> v[i];
+	}
+
+	cin >> a >> b;
+	ans = N;
+	for (int i = 0; i < N; i++) {
+		int tmp = v[i] - a;
+
+		if (tmp > 0) { //continue 보다는 if문 쓰기
+			ans += ceil((double)tmp / b);
+		}
+	}
+	cout << ans << endl;
+}
