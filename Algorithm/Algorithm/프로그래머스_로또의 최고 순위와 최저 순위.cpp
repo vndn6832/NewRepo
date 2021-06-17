2666e8a3d4566dcf519870bf444da1ce27638c7b
yoori choi <vndn6832@likelion.org>
2021-06-17 23:26:16 +0900

    프로그래머스 로또의 최고 순위와 최저 순위

---
+++ Algorithm/Algorithm/프로그래머스_로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer;
+    sort(lottos.begin(), lottos.end());
+    sort(win_nums.begin(), win_nums.end());
+    
+    int zero = 0;
+    int start = 0;
+    int same = 0;
+    for(int i=0;i<lottos.size();i++){
+        if(lottos[i] == 0){
+            zero++;
+            continue;
+        }
+        
+        
+        for(int j=start;j<win_nums.size();j++){
+            if(lottos[i] == win_nums[j] ){
+                same++;
+                break;
+            }
+        }
+            
+    }
+    
+
+    
+    if(same+zero == 1 || same+zero == 0)
+        answer.push_back(6);
+    else
+        answer.push_back(7-(same+zero));
+    
+    if(same == 1 || same == 0)
+        answer.push_back(6);
+    else
+        answer.push_back(7-same);
+    
+    
+    
+    
+    return answer;
+}
+
+void main(){
+    solution( {44,1,0,0,31,25},{31,10,45,1,6,19});
+}
